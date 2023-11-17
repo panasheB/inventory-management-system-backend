8d4f21a26bf62a4ce253a6b1c33f7a3d9f0b63a4
Taboz <mafuratavonga@gmail.com>
2023-11-17 22:12:09 +0200

    add check code in items

--- routes/items_router.js
+++ routes/items_router.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const ItemsMOdel = require("../models/items_model");
+const ItemsModel = require("../models/items_model");
 
 // Getting all
 router.get("/get", async (req, res) => {
   try {
-    const items = await ItemsMOdel.find();
+    const items = await ItemsModel.find();
     res.json(items);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -17,7 +17,7 @@ router.delete("/deleteItem", async (req, res) => {
   const { code } = req.body; 
 
   try {
-    const deleteItel = await ItemsMOdel.findOneAndDelete({ code: code });
+    const deleteItel = await ItemsModel.findOneAndDelete({ code: code });
 
     if (!deleteItel) {
       // return res.status(404).json({ message: "Item not found" });
@@ -34,7 +34,7 @@ router.put("/updateItemQuantity", async (req, res) => {
   const { code, quantity } = req.body; // Assuming you are sending the code and quantity from the frontend.
   try {
     // Find the item by its code.
-    const item = await ItemsMOdel.findOne({ code });
+    const item = await ItemsModel.findOne({ code });
     if (!item) {
     }
     const newQuantityAvailable = item.availableQuantity + quantity;
@@ -47,12 +47,29 @@ router.put("/updateItemQuantity", async (req, res) => {
 
 // Creating one
 router.post("/create", async (req, res) => {
-  const item = new ItemsMOdel(req.body);
+  const item = new ItemsModel(req.body);
   try {
     const newItem = await item.save();
     res.status(201).json(newItem);
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+});
+
+// Check if item code exists
+router.get("/check/:code", async (req, res) => {
+  const { code } = req.params;
+
+  try {
+    const item = await ItemsModel.findOne({ code });
+
+    if (item) {
+      res.json({ exists: true });
+    } else {
+      res.json({ exists: false });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error", error: err.message });
   }
 });
 
